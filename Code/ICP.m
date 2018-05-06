fb9249e4a6d3782be1020f491c774493ffea0b3c
Chaojun Li <chaojunli@Chaojuns-MacBook-Pro.local>
2018-05-05 20:51:04 -0400

    add ICP

--- Code/ICP.m
+++ Code/ICP.m
@@ -3,6 +3,8 @@ function [out] = ICP(Pts1, Pts2)
 %   Detailed explanation goes here
 %%
 %temp
+PTSClouds =  load('PointClouds_scene_001.mat');
+PTSClouds = PTSClouds.PTSClouds;
 Pc1 = PTSClouds{1};
 Pc2 = PTSClouds{2};
 
@@ -10,28 +12,33 @@ Pts1 = Pc1.Location;
 Pts1(any(isnan(Pts1),2),:) = [];
 
 
-Pts2 = Pc1.Location;
+Pts2 = Pc2.Location;
 Pts2(any(isnan(Pts2),2),:) = [];
 
 
 
 max_iter = 1000;
 thresh = 0.001;
+dist = 9999999;
 R = [1 0 0; 0 1 0; 0 0 1];
 t = [0, 0, 0];
 
 tree = KDTreeSearcher(Pts1);
 
- for i=1:1
+ while i < 500 && dist > thresh
      mapping = knnsearch(tree, Pts2,'K',3);
      map = mapping(:,1);
      dist = sqrt(sum((Pts2 - Pts1(map,:)) .^2,2));
-     dist = mean(dist);
-     
-     
+     dist = mean(dist)
+     [Ri,ti] = rigidTransform(Pts2,Pts1,map);
+     Pts2 = Ri*Pts2'+ti;
+     Pts2 = Pts2';
+     R = Ri*R;
+     t = Ri*ti+t;
  end
 
 %% 
+
  mapping_sub = mapping(1:500,:);
  mapping_sub = mapping_sub(:,1);
  
